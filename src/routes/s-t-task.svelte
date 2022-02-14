2c41d04126d903561a46548a8ed5e94e1df9a097
EmixD <yakovkinii@gmail.com>
2022-02-14 21:49:14 +0200

    task styling

--- src/routes/s-t-task.svelte
+++ src/routes/s-t-task.svelte
@@ -1,7 +1,7 @@
 <script>
     import Fa from 'svelte-fa'
     import { faTrashCan } from '@fortawesome/free-solid-svg-icons/faTrashCan'
-
+    import { fade, fly } from 'svelte/transition';
     export let task;
 	import { createEventDispatcher } from 'svelte';
 
@@ -9,28 +9,35 @@
     function del(){
         dispatch('message',{command:"removeTask",id:task.id});
     }
+    let showui=false;
 </script>
 
 <div class="yys-wbp-hbc yycc shadow colortaskfuture task" style="
-display: flex;
-flex-direction: row;
-justify-content: space-between;
+display: grid;
+grid-template-columns: auto 1fr auto;
+grid-template-rows: auto;
 gap: 10px;
 padding-right: 15px;
-">
-    <div class="yysbc yycc time shadowtext">
+"
+on:mouseenter={()=>{showui=true}}
+on:mouseleave={()=>{showui=false}}
+>
+    <div class="yysbc yycc time shadowtext yynoselect">
         <p style="width: max-content; padding: 5px;">
             {task.timestring?task.timestring:"00:00"}
         </p>
     </div>
-    <div class="yysg title shadowtext">
+    <div class="yysp title shadowtext yynoselect" style="overflow-x: hidden;">
         <p class="yysbс" style="padding: 5px;">
-            {task.text}
+            <nobr>{task.text}</nobr>
         </p>
     </div>
-    <div class="yysf yycc yynoselect shadowtext" on:click={del}>
-        <Fa size="1.5x" icon={faTrashCan}/>
-    </div>
+    {#if showui}
+        <div class="yysf yycc yynoselect shadowtext" 
+        on:click={del} out:fade>
+            <Fa size="1.5x" icon={faTrashCan}/>
+        </div>
+    {/if}
 </div>
 
 <style>
